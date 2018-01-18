761b5241ff9b1385df3522475cf05a301000f84c
mateusz <mzalewski1992@gmail.com>
2018-01-18 23:43:48 +0100

    fetch_repo - checking status code

--- fetch_repo.sh
+++ fetch_repo.sh
@@ -1,9 +1,25 @@
 #!/bin/bash
 
+# Fetch last modified repository from github
+
+clear
+
+API_URL='https://api.github.com/users/'$1'/repos?sort=updated&per_page=1'
+
+API_QUERY=$(curl -s $API_URL)
+
+RESPONSE_CODE=$(curl -s -o /dev/null -w '%{http_code}' $API_URL;)
+
+
 if [ -z "$1" ]; 
 then
 	echo "Type repo name as parameter"
 else
-	echo "Fetching data for $1"
-	curl -s "https://api.github.com/users/$1/repos?sort=updated&per_page=1" | jq -c '.[] | .name'
+	if [[ $RESPONSE_CODE -eq 200 ]]; 
+	then
+		echo "Fetching data for $1 Response code: $RESPONSE_CODE"
+		echo $API_QUERY | jq -c '.[] | .name'
+	else
+		echo "Repository not found :("
+	fi
 fi
